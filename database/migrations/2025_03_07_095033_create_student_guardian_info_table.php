59597cf73ae01cac44d19eeb6db998960c66b070
Prakash Pokhrel <prakash@edigitalnepal.edu.np>
2025-03-07 17:25:08 +0545

    added database migration files of student,student_education_history,student gurdians

---
+++ database/migrations/2025_03_07_095033_create_student_guardian_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_guardian_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('type')->nullable();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->boolean('current_guardian')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_guardian_info');
+    }
+};
