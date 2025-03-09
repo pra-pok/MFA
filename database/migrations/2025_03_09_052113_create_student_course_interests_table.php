0eb1f9b28433f000cf27191b1a9a7fe1f2512ad9
Prakash Pokhrel <prakash@edigitalnepal.edu.np>
2025-03-09 11:28:57 +0545

    added new migartion for student course interest

---
+++ database/migrations/2025_03_09_052113_create_student_course_interests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_course_interests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('agreement_amount')->nullable();
+            $table->text('remarks')->nullable();
+            $table->boolean('current')->default(true);
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_course_interests');
+    }
+};
