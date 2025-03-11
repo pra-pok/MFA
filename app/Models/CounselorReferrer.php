9d5798364f0cf1b7b2dd29f7f792d12ff5eb1cad
Prakash Pokhrel <prakash@edigitalnepal.edu.np>
2025-03-11 17:11:48 +0545

    managed student api fro multiple counselor id added case

--- app/Models/CounselorReferrer.php
+++ app/Models/CounselorReferrer.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\StudentCounselorReffer;
 
 class CounselorReferrer extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    protected $table = 'counselor_referrers';
     protected $fillable = [
         'name',
         'email',
@@ -19,11 +21,23 @@ class CounselorReferrer extends Model
         'created_by',
         'updated_by',
     ];
-    protected $table = 'counselor_referrers';
+
+    
     public function createds(){
         return $this->belongsTo(OrganizationSignup::class,'created_by','id');
     }
     public function updatedBy(){
         return $this->belongsTo(OrganizationSignup::class,'updated_by','id');
     }
+
+    public function students()
+    {
+        return $this->belongsToMany(
+            Student::class,
+            'student_counselor_referrers',
+            'counselor_referred_id',
+            'student_id'
+        );
+    }
+
 }
